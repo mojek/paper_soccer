2261d6922c0eefd517c8ef26e1ab02b879c60d2a
Michal Mojek <m.mojek@gmail.com>
2021-09-13 21:04:40 +0200

    Refactor tests

--- playing/spec/start_match_spec.rb
+++ playing/spec/start_match_spec.rb
@@ -6,31 +6,26 @@ RSpec.describe Playing::MatchStarted do
     Rails.configuration.command_bus
   end
 
-  def event_store
-    # RubyEventStore::Client.new(repository: RubyEventStore::InMemoryRepository.new)
-    # Rails.configuration.event_store
-    # end
-    Rails.configuration.event_store
+  def start_match_command(**kwargs)
+    params = { aggregate_id: 1, user_id: 10, game_session_id: 1,
+               match_id: 42, width: 8, height: 12 }
+    command_bus.call(Playing::StartMatch.new(params.merge(kwargs)))
+  end
 
+  def event_store
+    Rails.configuration.event_store
   end
 
   it 'Start Match Create Successfully' do
-    command_bus.call(Playing::StartMatch.new(aggregate_id: 1, user_id: 10, game_session_id: 1,
-                                             match_id: 42, width: 8, height: 12))
+    start_match_command
     expect(event_store).to have_published(an_event(Playing::MatchStarted))
   end
 
   it 'Raise Error when FootballPitchToSmall' do
-    expect do
-      command_bus.call(Playing::StartMatch.new(aggregate_id: 1, user_id: 10, game_session_id: 1,
-                                               match_id: 42, width: 6, height: 2))
-    end.to raise_error(Playing::Play::FootballPitchTooSmall)
+    expect { start_match_command(width: 6, height: 2) }.to raise_error(Playing::Play::FootballPitchTooSmall)
   end
 
   it 'Raise Error when FootballPitchOddSize' do
-    expect do
-      command_bus.call(Playing::StartMatch.new(aggregate_id: 1, user_id: 10, game_session_id: 1,
-                                               match_id: 42, width: 11, height: 15))
-    end.to raise_error(Playing::Play::FootballPitchOddSize)
+    expect { start_match_command(width: 11, height: 15) }.to raise_error(Playing::Play::FootballPitchOddSize)
   end
 end
